c66b66a7c43c46729127dbcbf9a679a4e8cfb800
gmth <143313748+gmth6789@users.noreply.github.com>
2023-10-30 04:43:31 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const serveStatic = require("serve-static");
 
 const app = express();
 app.use(serveStatic(__dirname));
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use("/", serveStatic(path.join(__dirname, "/public")));
 app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 const port = process.env.PORT || 8080;
 app.listen(port);
